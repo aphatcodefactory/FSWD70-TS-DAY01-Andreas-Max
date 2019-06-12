91e7ee4616cd60dfa588fed5842eeb1ffe2d47ab
Max <maximilian@dirninger.com>
2019-06-12 14:38:53 +0200

    created restaurant folder

--- Max/Challenge/script.js
+++ Max/Challenge/script.js
@@ -1,49 +1,33 @@
 // Define for each variable/array the data type
-	
-
 // List the array items and make sure that it has a strict data type definition
-
-
 // Fetch Array items by employing Array Destructuring concept
-
-
 // Provide the functionality for giving a comment for some product
-
-
 // Provide the functionality for giving a rating for some product and increment/decrement it
-
-
 // Use some loop for displaying the data into the Page (forEach, For in or For-of)
-
-
 // Define some appropriate value as const
-
-
 // Define for each variable/array the data type
-
-
 var nummer = [];
+var _loop_1 = function () {
+    var y = i;
+    nummer.push(function (a) {
+        // console.log(y);
+        // let z: any = y + " x " + a + " = " + y * a + "<br><br>";
+        document.write(y + " x " + a + " = " + y * a + "<br><br>");
+        // document.getElementById("test").innerHTML = z;
+    });
+};
 for (var i = 1; i < 11; i++) {
-	let y: number = i;
-	nummer.push(function(a) {
-		// console.log(y);
-		
-		// let z: any = y + " x " + a + " = " + y * a + "<br><br>";
-		document.write(y + " x " + a + " = " + y * a + "<br><br>");
-		
-		// document.getElementById("test").innerHTML = z;
-	})
+    _loop_1();
 }
-nummer.forEach(function(rechnung) {
-	rechnung(1)
-	rechnung(2)
-	rechnung(3)
-	rechnung(4)
-	rechnung(5)
-	rechnung(6)
-	rechnung(7)
-	rechnung(8)
-	rechnung(9)
-	rechnung(10)
-
+nummer.forEach(function (rechnung) {
+    rechnung(1);
+    rechnung(2);
+    rechnung(3);
+    rechnung(4);
+    rechnung(5);
+    rechnung(6);
+    rechnung(7);
+    rechnung(8);
+    rechnung(9);
+    rechnung(10);
 });
